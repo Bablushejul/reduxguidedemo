8288acd6d811c7677edcc629948ca051f870274a
Bablushejul <shejulbablu@gmail.com>
2023-05-01 18:39:31 +0530

    counter

--- src/Store/indexRedux.js
+++ src/Store/indexRedux.js
@@ -1,19 +1,50 @@
 import { createStore } from "redux";
+import { createSlice } from "@reduxjs/toolkit";
 
-const counterReducer=(state={counter:0},action)=>{
+
+
+const initialState={counter:0,showCounter:true}
+
+createSlice({
+    name:'counter',
+    initialState,
+    reducers:{
+        increment(state){
+            state.counter++;
+        },
+        decrement(state){
+            state.counter--
+        },
+        toggleCounter(state){
+            state.showCounter=!state.showCounter
+        }
+    }
+})
+
+const counterReducer=(state=initialState,action)=>{
 
     if(action.type==='increment'){
        
         return {
-            counter:state.counter+1
+            counter:state.counter+1,
+            showCounter:state.showCounter
         };
     }
 
     if(action.type==='decrement'){
         return {
-            counter:state.counter-1
+            counter:state.counter-1,
+            showCounter:state.showCounter
         }
     }
+    if(action.type==='toggle'){
+        return {
+            showCounter:!state.showCounter,
+            counter:state.counter
+
+        }
+    }
+
     return state;
 }
 
